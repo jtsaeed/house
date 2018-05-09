10dd6cb7ddb9a16c67e1357ad83e2c65722e16c6
James T Saeed <business@evh98.com>
2018-05-09 16:17:00 +0100

    Initial Commit

---
+++ house/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  house
+//
+//  Created by James Saeed on 09/05/2018.
+//  Copyright © 2018 James Saeed. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
