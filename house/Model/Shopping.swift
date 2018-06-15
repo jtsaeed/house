f89844c1216d5eaf17cce808cca77e4ade7f8168
James T Saeed <business@evh98.com>
2018-06-15 15:15:44 +0400

    #29

---
+++ house/Model/Shopping.swift
@@ -0,0 +1,20 @@
+//
+//  Chore.swift
+//  house
+//
+//  Created by James Saeed on 19/05/2018.
+//  Copyright © 2018 James Saeed. All rights reserved.
+//
+
+import Foundation
+
+struct Shopping {
+    let shoppingId: String
+    
+    let content: String
+    let author: String
+    
+    func clearItem() {
+        DataService.instance.deleteShoppingItem(with: shoppingId)
+    }
+}
